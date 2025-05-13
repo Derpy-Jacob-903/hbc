6c2d68e1ee5159fb5f24fef11ad90a63c36c52aa
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2025-05-12 20:39:10 -0500

    stuff for imf mod

---
+++ healthAttack.gd
@@ -0,0 +1,23 @@
+extends SigilEffect
+
+# c# Version
+##int damageout = 0;
+##if (base.PlayableCard && base.PlayableCard.Health > 0)
+##	{
+##		damageout = base.PlayableCard.Health;
+##	}
+##return new int[]
+##	{
+##		damageout,	# Damage...
+##		0			# and Health
+##	};
+
+#Used for sigils that passively define the power of the card they're attached to, such as Ant, Spilled Blood, etc...
+func define_power():
+	print("we are defining power this one!!")
+	var damageout = 0
+	if card and card.health > 0:
+		damageout = card.health
+		print("Bloon Health:", card.health)
+	return damageout
+
