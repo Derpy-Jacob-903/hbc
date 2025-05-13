8fb381a42356c3911f50bbb11dde9877025430c0
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2025-05-12 22:40:52 -0500

    "Oopsies..."
    "Forgot to remove is_power_defining() when it's no longer the method for using power defining sigils.
    
    Added the important comment information to all sigils that use the define_power() function that explains how you use sigils with that code"

--- healthAttack.gd
+++ healthAttack.gd
@@ -13,6 +13,8 @@ extends SigilEffect
 ##	};
 
 #Used for sigils that passively define the power of the card they're attached to, such as Ant, Spilled Blood, etc...
+#IMPORTANT! Sigils with this effect do not go with normal sigils, they must be put in the 'atkspecial' arguement.
+#Note that sigils in 'atkspecial' will be excluded from the normal sigil list(s), meaning none of their other functions will trigger.
 func define_power():
 	print("we are defining power with this one!!")
 	var damageout = 0
