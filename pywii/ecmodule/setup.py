1dd3eea26976f155717191eda194eae560a13a3c
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2025-05-12 21:10:10 -0500

    Delete pywii directory

--- pywii/ecmodule/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('_ec',
-                    sources = ['_ecmodule.c'])
-
-setup (name = 'ELT_C',
-       version = '0.1',
-       description = 'C ELT functions',
-       ext_modules = [module1])
